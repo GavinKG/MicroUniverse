78c23e2024805fd62b2b06985a44756200e5667e
Gavin_KG <gavin_kg@outlook.com>
2020-07-13 16:05:35 +0800

    finish TestCityWallGen test helper.

--- Assets/UnitTests/CityWallGen/Scripts/TestCityWallGen.cs
+++ Assets/UnitTests/CityWallGen/Scripts/TestCityWallGen.cs
@@ -2,31 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestCityWallGen : MonoBehaviour {
+namespace MicroUniverse {
 
-    public Texture2D cityTex;
+    public class TestCityWallGen : MonoBehaviour {
 
-    public GameObject coverGO;
-    public GameObject wallGO;
+        public enum ShrinkSize {
+            One, Two, Four, Eight, Sixteen
+        }
 
-    public float squareSize = 0.2f;
-    public float wallHeight = 2f;
 
-    public void Generate() {
+        public Texture2D cityTex;
+        public ShrinkSize shrink;
 
-        MeshFilter meshFilter = GetComponent<MeshFilter>();
-        bool[,] map = Texture2BoolMap.Convert(cityTex);
+        public GameObject coverGO;
+        public GameObject wallGO;
 
-        CityWallGenerator cityWallGenerator = new CityWallGenerator();
-        cityWallGenerator.GenerateMesh(map, squareSize, wallHeight);
+        public float wallLength = 20f;
+        public float wallHeight = 2f;
+        [Range(0, 4)]
+        public int smoothCount = 2;
 
-        coverGO.transform.position = Vector3.zero;
-        coverGO.transform.rotation = Quaternion.identity;
-        wallGO.transform.position = Vector3.zero;
-        wallGO.transform.rotation = Quaternion.identity;
+        public void Generate() {
 
-        coverGO.GetComponent<MeshFilter>().mesh = cityWallGenerator.CoverMesh;
-        wallGO.GetComponent<MeshFilter>().mesh = cityWallGenerator.WallMesh;
+            RenderTexture rt;
+
+            int newWidth = cityTex.width, newHeight = cityTex.height;
+
+            if (shrink == ShrinkSize.Two) {
+                newWidth = cityTex.width / 2;
+                newHeight = cityTex.height / 2;
+            } else if (shrink == ShrinkSize.Four) {
+                newWidth = cityTex.width / 4;
+                newHeight = cityTex.height / 4;
+            } else if (shrink == ShrinkSize.Eight) {
+                newWidth = cityTex.width / 8;
+                newHeight = cityTex.height / 8;
+            } else if (shrink == ShrinkSize.Sixteen) {
+                newWidth = cityTex.width / 16;
+                newHeight = cityTex.height / 16;
+            }
+
+            rt = new RenderTexture(newWidth, newHeight, 0);
+            Graphics.Blit(cityTex, rt);
+            bool[,] map = Util.Tex2BoolMap(rt);
+
+            MeshFilter meshFilter = GetComponent<MeshFilter>();
+
+            CityWallGenerator cityWallGenerator = new CityWallGenerator();
+            cityWallGenerator.GenerateMesh(map, wallLength / newWidth, wallHeight, smoothCount);
+
+            coverGO.transform.position = Vector3.zero;
+            coverGO.transform.rotation = Quaternion.identity;
+            wallGO.transform.position = Vector3.zero;
+            wallGO.transform.rotation = Quaternion.identity;
+
+            coverGO.GetComponent<MeshFilter>().mesh = cityWallGenerator.CoverMesh;
+            wallGO.GetComponent<MeshFilter>().mesh = cityWallGenerator.WallMesh;
+
+            rt.Release();
+
+        }
 
     }
 
