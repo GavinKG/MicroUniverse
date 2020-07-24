c6d03a5c2e670c14fede7da366add61f9b77cafa
Gavin_KG <gavin_kg@outlook.com>
2020-07-25 01:01:23 +0800

    FPS Counter added.

---
+++ Assets/Scripts/FPSCounter.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FPSCounter : MonoBehaviour {
+
+    public Text fpsText;
+    public float refreshRate = 1f;
+
+    private float timer;
+
+    private void Update() {
+        if (Time.unscaledTime > timer) {
+            int fps = (int)(1f / Time.unscaledDeltaTime);
+            fpsText.text = "FPS: " + fps;
+            timer = Time.unscaledTime + refreshRate;
+        }
+    }
+}
