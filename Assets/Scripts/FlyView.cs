05382cbb1ec541c2c8fd96aca8683162449bf2b7
Gavin_KG <gavin_kg@outlook.com>
2020-07-27 00:04:42 +0800

    Flyview & LightRotator & InputSystem for debugging and presenting.

---
+++ Assets/Scripts/FlyView.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlyView : MonoBehaviour {
+    public float rotationSpeed = 5f;
+    public float movementSpeed = 1f;
+
+    Vector2 inputMovementAxis;
+    Vector2 inputAimAxis;
+    float upDownAxis;
+    public bool speedup;
+
+    public void OnPlayerMovementInput(InputAction.CallbackContext context) {
+        inputMovementAxis = context.ReadValue<Vector2>();
+    }
+
+    public void OnPlayerAimInput(InputAction.CallbackContext context) {
+        inputAimAxis = context.ReadValue<Vector2>();
+    }
+
+    public void OnPlayerSpeedupInput(InputAction.CallbackContext context) {
+        if (context.started) {
+            speedup = true;
+        } else if (context.canceled) {
+            speedup = false;
+        }
+    }
+
+    public void OnPlayerUpDownInput(InputAction.CallbackContext context) {
+        upDownAxis = context.ReadValue<float>();
+    }
+
+    private void Update() {
+
+        Vector3 rot = transform.eulerAngles;
+        rot.y += inputAimAxis.x * rotationSpeed * Time.deltaTime;
+        rot.x += -inputAimAxis.y * rotationSpeed * Time.deltaTime;
+        transform.eulerAngles = rot;
+
+        Vector3 currAiming = transform.rotation * Vector3.forward;
+        Vector3 xzAimingN = new Vector3(currAiming.x, 0f, currAiming.z).normalized;
+        Vector3 rightN = Vector3.Cross(xzAimingN, Vector3.up);
+        Vector3 pos = transform.position;
+        pos += (inputMovementAxis.y * xzAimingN + inputMovementAxis.x * -rightN + upDownAxis * Vector3.up) * movementSpeed * Time.deltaTime * (speedup ? 2f : 1f);
+        transform.position = pos;
+    }
+}
---
+++ Assets/Scripts/LightRotator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MicroUniverse {
+    public class LightRotator : MonoBehaviour {
+
+        public Vector3 initialRotation;
+        
+        public float speed = 5f;
+
+        Vector3 currRotation;
+
+        void Start() {
+            currRotation = initialRotation;
+        }
+        
+        void Update() {
+            currRotation.y += speed * Time.deltaTime;
+            if (currRotation.y > 360f) {
+                currRotation.y = -360;
+            }
+            transform.eulerAngles = currRotation;
+        }
+    }
+
+}
