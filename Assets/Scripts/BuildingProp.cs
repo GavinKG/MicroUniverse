9e0a5309e5795513e92fd9df3fa12cb202dd5123
Gavin_KG <gavin_kg@outlook.com>
2020-07-30 00:21:28 +0800

    prepare props for destroying buildings.
    also added base prefab automation

--- Assets/Scripts/BuildingProp.cs
+++ Assets/Scripts/BuildingProp.cs
@@ -26,6 +26,34 @@ namespace MicroUniverse {
         public HeightType heightType;
         public BuildingType buildingType;
 
+        public GameObject baseGO;
+        public GameObject destroyedPrefab;
+
+        /// <summary>
+        /// Replace existing props (not base) to the new props from given prop prefab.
+        /// Old props will be destroyed; new props will be instantiated.
+        /// Used for replacing destroyed version of this building on the go.
+        /// </summary>
+        /// <param name="newProp"></param>
+        public void ReplaceBuildingProp(BuildingProp newProp) {
+
+            // Destroy
+            foreach (Transform t in transform) {
+                if (t.gameObject != baseGO) {
+                    Destroy(t.gameObject);
+                }
+            }
+
+            // Instantiate new
+            foreach (Transform t in newProp.transform) {
+                if (t.gameObject != newProp.baseGO) {
+                    Instantiate(t.gameObject, transform); // apply sqt or not?
+                }
+            }
+
+            // doesn't care whether baseGO is null or not!
+        }
+
 
     }
 }
--- Assets/Scripts/Editor/LoadingJobEditor.cs
+++ Assets/Scripts/Editor/LoadingJobEditor.cs
@@ -17,7 +17,35 @@ namespace MicroUniverse {
                 loadingJob.Load();
             }
 
+            if (GUILayout.Button("Bake all building prop prefab")) {
+                foreach (BuildingProp bp in loadingJob.propCollection.buildings) {
+                    EditorUtility.SetDirty(bp);
+                    foreach (Transform t in bp.transform) {
+                        if (t.name.Contains("Base")) {
+                            GameObject baseGO = t.gameObject;
+
+                            bp.baseGO = baseGO;
+
+                            bool meshesToTransformContainsThis = false;
+                            foreach (MeshFilter mf in bp.meshesToTransform) {
+                                if (mf.gameObject == baseGO) {
+                                    meshesToTransformContainsThis = true;
+                                    break;
+                                }
+                            }
+                            if (!meshesToTransformContainsThis) {
+                                MeshFilter mf = baseGO.GetComponent<MeshFilter>();
+                                if (mf == null) {
+                                    throw new System.Exception("mesh filter should be in base prefab's root transform.");
+                                }
+                                bp.meshesToTransform.Add(mf); 
+                            }
+                            break;
+                        }
+                    }
+                }
+                Debug.Log("Finished baking. Save to apply changes to prefab files.");
+            }
         }
     }
-
 }
