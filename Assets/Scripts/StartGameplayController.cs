e7aa705d876f377ba7141e8f8e67ab71bf3594ad
Gavin_KG <gavin_kg@outlook.com>
2020-08-02 22:03:07 +0800

    Kaleido GUI tweak

--- Assets/Scripts/StartGameplayController.cs
+++ Assets/Scripts/StartGameplayController.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace MicroUniverse {
     public class StartGameplayController : GameplayControllerBase {
 
         public KaleidoPainter painter;
+
+        public List<SpriteRenderer> spriteRenders;
 
         public override void Begin() {
             
@@ -13,6 +16,17 @@ namespace MicroUniverse {
 
         public override void Finish() {
             
+        }
+
+        public void OnColorBallClick() {
+            print("!");
+            GameObject buttonGO = UnityEngine.EventSystems.EventSystem.current.currentSelectedGameObject;
+            Image buttonImage = buttonGO.GetComponent<Image>();
+            Color color = buttonImage.color;
+            GameManager.Instance.CityWallColor = color;
+            foreach (SpriteRenderer spriteRenderer in spriteRenders) {
+                spriteRenderer.color = color;
+            }
         }
 
         public void OnPaintClick() {
