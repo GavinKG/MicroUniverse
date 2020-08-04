9ab0b69ef779a7c6285d2c6bf365f62ef8d70f45
Gavin_KG <gavin_kg@outlook.com>
2020-08-04 19:11:37 +0800

    Visual upgrade

---
+++ Assets/Scripts/Theme.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MicroUniverse {
+
+    public class Theme : MonoBehaviour {
+
+        public Color main;
+
+        public Color floor;
+        public Color buildingRoof;
+        public Color buildingBody;
+        public Color buildingFrame;
+        public Color buildingWindows;
+        public Color propBase;
+        public Color empty;
+        public Color plant;
+    }
+
+}
