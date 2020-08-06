931d38a0157b83ac0630fc4a89320ae9c5934209
Gavin_KG <gavin_kg@outlook.com>
2020-08-06 20:04:33 +0800

    citywall masked again!

---
+++ Assets/Scripts/MaterialKeywordActivator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialKeywordActivator : MonoBehaviour {
+
+    public Material mat;
+    public string keyword;
+   
+
+    public void On() {
+        mat.EnableKeyword(keyword);
+    }
+
+    public void Off() {
+        mat.DisableKeyword(keyword);
+    }
+}
