585833be04149d7b4aa5b33ac5bb956bba91e675
Gavin_KG <gavin_kg@outlook.com>
2020-08-07 15:27:08 +0800

    outro scene.

---
+++ Assets/Scripts/GameplayController/OutroGameplayController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+namespace MicroUniverse {
+
+    // Just play some timeline...
+    public class OutroGameplayController : GameplayControllerBase {
+
+        public Sprite canvasSprite;
+
+        PlayableDirector director;
+
+        [Header("Animations")]
+        public TimelineAsset outroTimeline;
+
+        public override void Begin() {
+
+            // prep ref:
+            director = GetComponent<PlayableDirector>();
+
+            // blend kaleido:
+            Texture original = canvasSprite.texture; // RW enabled tex.
+
+            director.Play(outroTimeline);
+            
+        }
+
+        public override void Finish() {
+
+        }
+
+        public void OnTimelineFinished() {
+            
+        }
+    }
+
+}
