ca9be505957dc6d3dc6b3e8e51069cdf1e865620
Gavin_KG <gavin_kg@outlook.com>
2020-08-07 23:13:38 +0800

    Kaleido colored!!!!!

--- Assets/Scripts/GameplayController/OutroGameplayController.cs
+++ Assets/Scripts/GameplayController/OutroGameplayController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 using UnityEngine.Timeline;
+using UnityEngine.UI;
 
 namespace MicroUniverse {
 
@@ -27,16 +28,31 @@ namespace MicroUniverse {
                 // blend kaleido:
                 Texture2D original = canvasSprite.texture; // RW enabled tex.
                 Texture2D colored = GameManager.Instance.ColoredTransparentTex;
-
-                // Color it on CPU:
-                Color32[] colors = original.GetPixels32();
+                colored = GaussianBlur.Blur(colored, 0, 0.5f, 1);
                 int width = original.width, height = original.height;
+                RenderTexture rt = RenderTexture.GetTemporary(width, height, 0);
+                RenderTexture prevRT = RenderTexture.active;
+                Graphics.Blit(colored, rt);
+                Texture2D colored1024 = Util.RT2Tex(rt);
+                RenderTexture.active = prevRT;
+                RenderTexture.ReleaseTemporary(rt);
                 
+                // Color it on CPU:
+                Color32[] originalColors = original.GetPixels32(); // should be in 1024x1024
+                Color32[] coloredColors = colored1024.GetPixels32();
 
+                
+                for (int x = 0; x < width; ++x) {
+                    for (int y = 0;  y < height; ++y) {
+                        int index = x * width + y;
+                        if (originalColors[index].a == 0) {
+                            originalColors[index] = coloredColors[index];
+                        }
+                    }
+                }
 
-
-            } else {
-                throw new System.Exception("?");
+                original.SetPixels32(originalColors);
+                original.Apply();
             }
 
 
